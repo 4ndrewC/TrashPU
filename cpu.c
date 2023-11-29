46db6384925bea38bbe580e872dde1ae2a7006a6
4ndrewC <andrewchengqq@yahoo.com>
2023-11-29 10:30:38 +0800

    more instructions + change flags

--- cpu.c
+++ cpu.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include "instructions.h"
 #include "memory.c"
+#include "flags.h"
 
 //mem[0] stores current rom pointer, mem[1] stores current ram pointer
 
@@ -12,7 +13,7 @@ word PC;
 word A, SP, P, X, Y;
 
 //status flags
-byte C, Z, I, D, B, O, N;
+byte flag;
 
 int mem[65536];
 
@@ -24,13 +25,6 @@ void CPUInit(){
     X = 0;
     Y = 0;
 
-    C = 0;
-    Z = 0;
-    I = 0;
-    D = 0;
-    B = 0;
-    O = 0;
-    N = 0;
 }
 
 int readMem(){
@@ -38,20 +32,15 @@ int readMem(){
     // printf("Y should equal this: ");
     // printf("%d", data);
     // printf("\n");
-    if(data==0){
-         Z = 1;
-         N = 0;
-    }
-    else if(data<=0){
-         N = 1;
-         Z = 0;
-    }
     return data; // return data being read given the address of ram memory
 }
 
-void writeMem(int data){
-    mem[SP] = data;
-    SP--;
+void writeMem(int data, word addr){
+    mem[addr] = data;
+}
+
+void setFlag(){
+
 }
 
 void execute(){;
@@ -97,7 +86,10 @@ void execute(){;
             // printf("Y here: ");
             // printf("%d", Y);
             // printf("\n");
-            writeMem(A);
+            Y = readMem();
+            writeMem(A, Y);
+            SP--;
+
             break;
         
         case STX:
@@ -108,7 +100,10 @@ void execute(){;
             printf("\n");
             mem[0]++;
             // printf("increment mem[0]\n");
-            writeMem(X);
+            Y = readMem();
+            writeMem(X, Y);
+            SP--;
+
             break;
         
         case STY:
@@ -119,9 +114,84 @@ void execute(){;
             printf("\n");
             mem[0]++;
             // printf("increment mem[0]\n");
-            writeMem(Y);
+            X = readMem();
+            writeMem(Y, X);
+            SP--;
+
             break;
         
+        case TAX:
+
+            X = A;
+            if(X==0){
+                flag = Z;
+            }
+            else if(X<=0){
+                flag = N;
+            }
+            break;
+        
+        case TAY:
+            Y = A;
+            if(Y==0){
+                flag = Z;
+            }
+            else if(Y<=0){
+                flag = N;
+            }
+            break;
+        
+        case TXA:
+            A = X;
+            if(A==0){
+                flag = Z;
+            }
+            else if(A<=0){
+                flag = N;
+            }
+            break;
+        
+        case TYA:
+            A = Y;
+            if(A==0){
+                flag = Z;
+            }
+            else if(A<=0){
+                flag = N;
+            }
+            break;
+        
+        case TSX:
+            X = SP;
+            if(X==0){
+                flag = Z;
+            }
+            else if(X<=0){
+                flag = N;
+            }
+            break;
+        case TXS:
+            SP = X;
+        case PHA:
+            SP--;
+            mem[SP] = A;    
+            break;
+        case PHP:
+            SP--;
+            mem[SP] = flag;
+            break;
+        case PLA:
+            A = mem[SP];
+            if(A==0){
+                flag = Z;
+            }
+            else if(A<=0){
+                flag = N;
+            }
+            break;
+        case PLP:
+            flag = mem[SP];
+            break;
     }
 }
 
@@ -152,24 +222,26 @@ int main(){
     //rom section
     mem[0xC000] = 1;
     mem[0xC001] = 4;
-    mem[0xC002] = 0x10FF;
+    mem[0xC002] = 0x0210;
     mem[0xC003] = 3;
     mem[0xC004] = 5;
-    mem[0xC005] = 0x10FE;
+    mem[0xC005] = 0x0211;
     mem[0xC006] = 2;
     mem[0xC007] = 6;
-    mem[0xC008] = 0x10FD;
+    mem[0xC008] = 0x0212;
 
     //initialize zero page address pointers
     mem[0] = 0xC000; 
     mem[1] = 0x0200; 
+
     fetch();
     fetch();
     fetch();
     fetch();
     fetch();
     fetch();
-    printf("%d", mem[SP+1]);
+    
+    printf("%d", mem[0x0211]);
     // printf("\n");
     // printf("%d", SP);
     
---
+++ flags.h
@@ -0,0 +1,14 @@
+#ifndef FLAGS_H
+#define FLAGS_H
+
+// define instruction and respective addresses
+#define C 1
+#define Z 2
+#define I 3
+#define D 4
+#define B 5
+#define O 6
+#define N 7
+
+
+#endif
